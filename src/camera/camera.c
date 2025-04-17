364d70ee88c10a0f83ae6a1cb453e63e6ee124db
Mart de Bruijn <h.b.g.d.bruijn@student.tue.nl>
2025-04-17 11:46:17 +0200

    Clean up of code and adding comments

--- src/camera/camera.c
+++ src/camera/camera.c
@@ -57,12 +57,10 @@ void readCameraData
     else if ( strcmp( label , SAMPLE ) == 0 )
     {
       fscanf( fin , "%d" , &cam->samples_per_pixel );
-      printf("DEBUG: Read samples_per_pixel = %d\n", cam->samples_per_pixel);
     }
     else if ( strcmp( label , STRATIFIED ) == 0 )
     {
       fscanf( fin , "%d" , &cam->strat );
-      printf("DEBUG: Read stratified = %d\n", cam->strat);
     }
     else if (strcmp(label, APERTURE) == 0) {
       fscanf(fin, "%le", &cam->aperture);
@@ -80,6 +78,12 @@ void readCameraData
   printf("    Rotation ................ : %f %f %f \n", cam->tilt.x , cam->tilt.y , cam->tilt.z);
   printf("    Field Of View ........... : %f\n",cam->fov);
   printf("    Number of samples ........: %d \n", cam->samples_per_pixel);
+  printf("    Sampling, 1 for stratified, 0 for random = %d\n", cam->strat);
+  printf("    Camera aperture = %d\n", cam->aperture);
+  printf("    Camera focal length = %d\n", cam->focal_length);
+
+
+
   printf("\n");  
 }
 
@@ -122,10 +126,10 @@ void generateRay
     double        v   ,
     CameraData*   cam )
 
-// {for (int sample = 0; sample < cam->samples_per_pixel; sample ++)
   {
   double lens_radius = 0.5 * cam->aperture;
   double r1, r2;
+  // create disc with random point r1 and r2 on it.
   do {
     r1 = 2.0 * rand() / (double)RAND_MAX - 1.0;
     r2 = 2.0 * rand() / (double)RAND_MAX - 1.0;
@@ -133,30 +137,20 @@ void generateRay
 
   double lens_x = lens_radius * r1;
   double lens_y = lens_radius * r2;
-    // double u_offset = rand()%1000 / 1000;
-  // double v_offset = rand()%1000 / 1000;
   
+  //convert input angles to radians
   double yaw    = cam->tilt.y * PICONST / 180;
   double pitch  = cam->tilt.x * PICONST / 180;
   double roll   = cam->tilt.z * PICONST / 180;
 
+  // calculate cosine and sine of angles
   double cy = cos(yaw),     sy = sin(yaw);
   double cx = cos(pitch), sx = sin(pitch);
   double cz = cos(roll),   sz = sin(roll);
 
-
-  // double cosY = cos(cam->tilt.y * PICONST / 180);
-  // double sinY = sin(cam->tilt.y * PICONST / 180);
-
-  // double cosX = cos(cam->tilt.x * PICONST / 180);
-  // double sinX = sin(cam->tilt.x * PICONST / 180);
   ray->o = cam->origin;
 
-  // ray->o.x = cam->origin.x * cosY - cam->origin.z * sinY;
-  // // printf("%f %f \n",cam->origin.x , ray->o.x);
-  // ray->o.y = cam->origin.y * cosX - cam->origin.z * sinX;
-  // ray->o.z = cam->origin.x * sinY * sinX + cam->origin.z * cosY * cosX;
-  
+  // Define 3x3 rotation matrix
   double R[3][3];
 
   R[0][0] = cy * cz;
@@ -176,38 +170,32 @@ void generateRay
   double dy = cam->y0-(ix+u)*cam->dx;
   double dz = cam->z0+(iy-v)*cam->dx;
 
+  // set the direction of the rays using the lens
   double dir_x = dx * cam->focal_length;
   double dir_y = dy * cam->focal_length;
   double dir_z = dz * cam->focal_length;
-  // double x1 = dx;
-  // double y1 = dy * cosPitch - dz *sinPitch;
-  // double z1 = dy * sinPitch + dz * cosPitch;
-
-  // ray->d.x = R[0][0]*dx + R[0][1]*dy + R[0][2]*dz;
-  // ray->d.y = R[1][0]*dx + R[1][1]*dy + R[1][2]*dz;
-  // ray->d.z = R[2][0]*dx + R[2][1]*dy + R[2][2]*dz;
-
-  // ray->d.x = dx*cosY -dz * sinY;
-  // ray->d.y = dy*cosX -dz * sinX;
-  // ray->d.z = dx * sinY * sinX + dz *cosY * cosX;
+  
 // Rotate direction
 double dir_rot_x = R[0][0]*dir_x + R[0][1]*dir_y + R[0][2]*dir_z;
 double dir_rot_y = R[1][0]*dir_x + R[1][1]*dir_y + R[1][2]*dir_z;
 double dir_rot_z = R[2][0]*dir_x + R[2][1]*dir_y + R[2][2]*dir_z;
 
-// Offset by lens
+// Calculate the offset by the lens
 double offset_x = R[0][0]*lens_x + R[0][1]*lens_y;
 double offset_y = R[1][0]*lens_x + R[1][1]*lens_y;
 double offset_z = R[2][0]*lens_x + R[2][1]*lens_y;
 
+// Set the origin of the ray equal to that of the camera plus the lens
 ray->o.x = cam->origin.x + offset_x;
 ray->o.y = cam->origin.y + offset_y;
 ray->o.z = cam->origin.z + offset_z;
 
+// Determine the direction of the rays
 ray->d.x = dir_rot_x - offset_x;
 ray->d.y = dir_rot_y - offset_y;
 ray->d.z = dir_rot_z - offset_z;
 
+// normalize the vectors
   double length = sqrt(ray->d.x * ray->d.x + ray->d.y * ray->d.y + ray->d.z * ray->d.z);
   ray->d.x /= length;
   ray->d.y /= length;
